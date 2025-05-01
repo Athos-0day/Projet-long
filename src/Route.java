6d77ee95ab1d0e6983cc8fd9e8d084c4f5a9d0c1
Athos-0day <athos.0day@protonmail.com>
2025-05-01 14:22:32 +0200

    nettoyage

--- src/Route.java
+++ src/Route.java
@@ -1,8 +1,9 @@
-/** La classe Route représente une route avec ses caractéristiques principales.
+/**
+ * La classe Route représente une route avec ses caractéristiques principales.
+ * Elle est définie par une position de début, une position de fin et une éventuelle signalisation.
  * 
  * @author Saadi Mohamed et Fatihi Abderrahman
  */
-
 public class Route {
     /** Position du début de la route */
     private Position positionDebut;
@@ -11,58 +12,69 @@ public class Route {
     /** Signalisation qui peut être présente sur la route. */
     private Signalisation signalisation;
 
-    /** Constructeur de Route. 
+    /**
+     * Constructeur de Route.
      * @param positionDebut Position du début de la route.
      * @param positionFin Position de la fin de la route.
      * @param signalisation Signalisation qui peut être présente sur la route.
-    */
-    public Route(Position positionDebut, Position positionFin,Signalisation signalisation) {
+     */
+    public Route(Position positionDebut, Position positionFin, Signalisation signalisation) {
         this.positionDebut = positionDebut;
         this.positionFin = positionFin;
         this.signalisation = signalisation;
     }
 
-    /** Permet de connaitre la position du début de la route. */
+    /** Retourne la position du début de la route. */
     public Position getPositionDebut() {
         return this.positionDebut;
     }
 
-    /** Permet de modifier la position du début de la route. */
-    public void setPostionDebut(Position positionDebut) {
+    /** Modifie la position du début de la route. */
+    public void setPositionDebut(Position positionDebut) {
         this.positionDebut = positionDebut;
     }
 
-    /** Permet de connaitre la position de la fin de la route. */
+    /** Retourne la position de la fin de la route. */
     public Position getPositionFin() {
         return this.positionFin;
     }
 
-    /** Permet de modifier la position de la fin de la route. */
-    public void setPostionDebut(Position positionFin) {
+    /** Modifie la position de la fin de la route. */
+    public void setPositionFin(Position positionFin) {
         this.positionFin = positionFin;
     }
 
-    /** Permet de connaitre la signalisation sur la route */
+    /** Retourne la signalisation présente sur la route. */
     public Signalisation getSignalisation() {
         return this.signalisation;
     }
 
-    /** Permet de modifier la signalisation sur la route. */
+    /** Modifie la signalisation sur la route. */
     public void setSignalisation(Signalisation signalisation) {
         this.signalisation = signalisation;
+    }
+
+    /**
+     * Calcule la longueur de la route en mètres.
+     * @return La distance entre le début et la fin de la route.
+     */
+    public double calculerLongueur() {
+        return this.positionDebut.calculerDistance(this.positionFin);
     }
 
     /**
      * Affiche les informations de la route.
      * @return Une chaîne de caractères représentant la route.
      */
+    @Override
     public String toString() {
         return "Route {\n" +
-                "  Position du début de la route: " + this.positionDebut.getAbscisse() +
-                 ", " + this.positionDebut.getOrdonee() + ")\n" +
-                "  Position de la fin de la route: (" + this.positionFin.getAbscisse() +
-                 ", " + this.positionFin.getOrdonee() + ")\n" +
-                " Signalisation sur la route: " + this.signalisation +
-                "}";
+               "  Position du début de la route: (" + this.positionDebut.getAbscisse() +
+               ", " + this.positionDebut.getOrdonee() + ")\n" +
+               "  Position de la fin de la route: (" + this.positionFin.getAbscisse() +
+               ", " + this.positionFin.getOrdonee() + ")\n" +
+               "  Longueur: " + calculerLongueur() + " mètres\n" +
+               "  Signalisation sur la route: " + this.signalisation +
+               "\n}";
     }
 }
