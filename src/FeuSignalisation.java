74504784bff3db609906bb3d5bcd96315cb7106f
MohibAlexandre <alexandremo@hotmail.fr>
2025-05-24 18:01:17 +0200

    Update FeuSignalisation.java

--- src/FeuSignalisation.java
+++ src/FeuSignalisation.java
@@ -1,24 +1,37 @@
 /** La classe Feu permet de définir les feux de signalisation présent sur la route.
  * 
- * @author Saadi Mohamed et Fatihi Abderrahman et Arthur Morain
+ * @author Saadi Mohamed et Fatihi Abderrahman et Arthur Morain et Mohib Alexandre
  */
 
-import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import java.awt.Color;
 
 public class FeuSignalisation {
     /** Position du feu de signalisation. */
     private int x, y;
-    /** Etat du feu (vert ou rouge). */
+    /** Etat du feu (vert ou rouge) */
     private boolean estVert;
-    /**Direction du feu (similaire aux véhicules). */
+    /** Direction du feu (similaire aux véhicules) */
     private int direction;
+    /** Images des deux formes de feu (rouge et vert) */
+    private Image imageFeuVert;
+    private Image imageFeuRouge;
+
+
+    // Chemins vers les images des feux
+    private static final String IMAGE_FEU_VERT = "ressources/images/feu_vert.png";
+    private static final String IMAGE_FEU_ROUGE = "ressources/images/feu_rouge.png";
 
     public FeuSignalisation(int x, int y, boolean estVert, int direction) {
         this.x = x;
         this.y = y;
         this.estVert = estVert;
         this.direction = direction;
+        // Charger les images des feux
+        this.imageFeuVert = new ImageIcon(IMAGE_FEU_VERT).getImage();
+        this.imageFeuRouge = new ImageIcon(IMAGE_FEU_ROUGE).getImage();
     }
 
     public void alterner() {
@@ -27,7 +40,7 @@ public class FeuSignalisation {
 
     // Méthode pour obtenir la position du feu sous forme d'un objet Point
     public Position getPosition() {
-        return new Position(this.x,this.y);  // Retourne les coordonnées sous forme de Point
+        return new Position(this.x, this.y);  // Retourne les coordonnées sous forme de Point
     }
 
     /** Méthode pour récupérer la direction du feu.
@@ -43,17 +56,24 @@ public class FeuSignalisation {
      */
     public void dessiner(Graphics g) {
         // Taille du feu de signalisation
-        int taille = 20;
-        
-        // Si le feu est vert, la couleur est verte, sinon rouge
-        if (estVert) {
-            g.setColor(Color.GREEN);
-        } else {
-            g.setColor(Color.RED);
-        }
+        int taille = 40;
 
-        // Dessiner le feu comme un cercle
-        g.fillOval(x, y, taille, taille);  // Dessiner un cercle pour le feu de signalisation
+        // Dessiner l'image du feu en fonction de son état
+        if (estVert) {
+            if (imageFeuVert != null) {
+                g.drawImage(imageFeuVert, x, y, taille, taille, null);
+            } else {
+                g.setColor(Color.GREEN);
+                g.fillOval(x, y, taille, taille);
+            }
+        } else {
+            if (imageFeuRouge != null) {
+                g.drawImage(imageFeuRouge, x, y, taille, taille, null);
+            } else {
+                g.setColor(Color.RED);
+                g.fillOval(x, y, taille, taille);
+            }
+        }
     }
 
     /** Méthode permettant de vérifier que le feu est au vert.
@@ -65,7 +85,7 @@ public class FeuSignalisation {
     }
 
     /** Calcule la position du point d'arrêt en fonction de la direction.
-     * @return la position du point d'arrêt 
+     * @return la position du point d'arrêt
      */
     public Position getPointArret() {
         int offset = 20; // distance devant le feu
